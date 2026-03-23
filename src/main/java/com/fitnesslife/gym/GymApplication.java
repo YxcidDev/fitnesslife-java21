6c1fea5e55ea9831ed915cc181ec80901fd378db
YxcidDev <ederyarevalo@gmail.com>
2026-03-22 23:33:56 -0500

    Revert "Merge pull request #7 from YxcidDev/eder"
    This reverts commit f261c05348b7d11709a4a399a6be40132ee61239, reversing
    changes made to c04e3e6e0bf2256757f6d850f7e6073a0325b320.

--- src/main/java/com/fitnesslife/gym/GymApplication.java
+++ src/main/java/com/fitnesslife/gym/GymApplication.java
@@ -5,19 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import jakarta.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @EnableScheduling
 @EnableRedisHttpSession
 @EnableMongoAuditing
 @SpringBootApplication
 public class GymApplication {
-
-	@PostConstruct
-	void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymApplication.class, args);
